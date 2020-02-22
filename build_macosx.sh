e637b027ee041e89aeed7c6b91c403188b141ea1
Tanguy Fautre <tanguy@fautre.com>
2020-02-22 20:28:48 +0000

    Adding tentative MacOS X build files (don't hit me, I don't have a Mac)

---
+++ build_macosx.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+
+mkdir --parents build/linux
+cd build/linux
+cmake -D VCPKG_TARGET_TRIPLET=x64-windows-static -D CMAKE_TOOLCHAIN_FILE=../vcpkg.windows.x64/scripts/buildsystems/vcpkg.cmake -D CMAKE_BUILD_TYPE=Release ../..
+make -j
---
+++ vcpkg_macosx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+mkdir -p build
+cd build
+git clone https://github.com/Microsoft/vcpkg.git vcpkg.macosx
+cd vcpkg.macosx
+git checkout 2020.01
+./bootstrap-vcpkg.sh
+
+./vcpkg install \
+	enet:x64-osx \
+	sdl2:x64-osx \
+	sdl2-mixer:x64-osx
